0bdb5609d3eea24ae3e2ea815cdaad560f6276c8
papuzinH <lautaro.hudson@ort.edu.ar>
2025-12-14 15:34:33 -0300

    feat: agregar soporte para archivos JavaScript en la configuración del middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,6 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     // Excluir archivos estáticos e imágenes para no gastar recursos
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|webmanifest)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|js|css|webmanifest)$).*)',
   ],
 }
