a487d90f616e476508c8fc5f8276ec6bf7b905eb
papuzinH <lautaro.hudson@ort.edu.ar>
2025-12-20 19:09:47 -0300

    feat: mejorar la lógica de redirección y manejo de errores en el middleware de autenticación

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,8 +12,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - auth/callback (EXCLUIR EXPLÍCITAMENTE)
      * - Any file with an extension (e.g. .svg, .png, .jpg, .webmanifest)
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|js|css|webmanifest)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|auth/callback|.*\\.(?:svg|png|jpg|jpeg|gif|webp|js|css|webmanifest)$).*)',
   ],
 }
